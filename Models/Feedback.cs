402ac9c5c9f760a37fbe05adf2e2c2ea763bd788
Andree Köhler <andkohler@gmail.com>
2025-06-05 20:54:21 +0200

    fixed connection between event and feedback

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,20 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FeedbackService.Models;
-
-public class Feedback
+namespace FeedbackService.Models
 {
-    public int Id { get; set; }
+    public class Feedback
+    {
+        public int Id { get; set; }
 
-    [Required]
-    public string EventTitle { get; set; } = null!;
+        [Required]
+        public string Name { get; set; } = null!;
 
-    [Required]
-    public string Category { get; set; } = null!;
+        [Required]
+        public DateTime Date { get; set; }
 
-    [Required]
-    public int Rating { get; set; }
+        [Required]
+        public int Rating { get; set; }
 
-    [Required]
-    public string Comment { get; set; } = null!;
+        [Required]
+        public string Comment { get; set; } = null!;
+
+        [Required]
+        public int EventId { get; set; }
+    }
 }
